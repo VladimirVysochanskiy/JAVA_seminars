7a880affe6ba0b16c51e14b327a75e8215e6c584
VladimirVysochanskiy <shunkar78@mail.ru>
2023-04-14 20:05:50 +0300

    homeTask03 changed

--- 01/homeTask03.java
+++ 01/homeTask03.java
@@ -1,9 +1,32 @@
 /**
- * homeTask03
+Task_3.
+Реализовать простой калькулятор (+,-,=,*), только с целыми числами.
  */
+import java.util.Scanner;
 public class homeTask03 {
 
     public static void main(String[] args) {
-        
+        Scanner iScanner = new Scanner(System.in);
+        System.out.println("\nКалькулятор\n");
+        System.out.println("Введите число A: ");
+        int a = iScanner.nextInt();
+        System.out.println("Введите операцию (+, -, *, /): ");
+        String op = iScanner.nextLine();
+        System.out.println("Введите число B: ");
+        int b = iScanner.nextInt();
+        iScanner.close();
+        switch (op) {
+            case "+":
+            System.out.printf("%d + %d = %d \n", a, b, a + b);
+            break;
+            case "-":
+            System.out.printf("%d - %d = %d \n", a, b, a - b);
+            break;
+            case "*":
+            System.out.printf("%d * %d = %d \n", a, b, a * b);
+            break;
+            case "/":
+            System.out.printf("%d / %d = %d \n", a, b, a / b);
+            break; }
     }
 }
